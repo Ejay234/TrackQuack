c03e5c504229ae9144105dc3fc73c2027a594b43
Ejay Aguirre <90516994+Ejay234@users.noreply.github.com>
2022-05-08 03:25:28 +1000

    Add files via upload

---
+++ business.js
@@ -0,0 +1,34 @@
+// Initiate the Openscreen node SDK and include the project ID that you recieved using the Openscreen dashboard 
+const { Openscreen } = require("@openscreen/sdk");
+require('dotenv').config();
+const os = new Openscreen().config({ key: process.env.OS_API_KEY, secret: process.env.OS_API_SECRET });
+const projectId = '9cb71d94-34ac-41ca-bbd3-1eb52c796f06';
+
+async function main() {
+
+    // Create an asset for a new listing sign being hosted for 123 Main Street 
+    const res = await os.project(projectId).assets().create({
+        name: 'PIC',
+        description: 'To track and show the business',
+        customAttributes: {
+            type: 'tracker'
+        },
+        qrCodes: [{
+            intent: 'https://www.picresorts.com/saipan/',
+            intentType: 'DYNAMIC_REDIRECT'
+        }],
+        
+    });
+
+    // Returns a scannable QR Code
+    const { qrCodeId } = res.asset.qrCodes[0];
+    const qrCode = await os.qrCode(qrCodeId).get({ format: 'png' })
+    await os.saveQrImageDataToFile(qrCode, 'PIC.png');
+
+    // View the new asset that you have created 
+    console.log('Asset:', JSON.stringify(res, '', 2));
+}
+
+main().catch((err) => {
+    console.error(err);
+});
